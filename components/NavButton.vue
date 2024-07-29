aae6e7ceba842df47cdb4dc98cf678f31b52f121
FallingSakura <dbw1732351264@163.com>
2024-07-29 16:51:44 +0800

    init

---
+++ components/NavButton.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: null,
+    },
+    url: {
+      type: String,
+      default: '/',
+    }
+  },
+  data() {
+    return {
+    }
+  },
+
+}
+</script>
+<template>
+  <button id="home"><a :href="url">{{ title }}</a></button>
+</template>
+
+<style scoped>
+#home {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  color: rgb(255, 255, 255);
+  background: rgba(71, 71, 71, 0.229);
+  padding: 8px, 10px;
+  border-radius: 5px;
+  left: 10px;
+  width: 18%;
+  font-size: 16px;
+}
+#home a {
+  text-align: center;
+}
+</style>
